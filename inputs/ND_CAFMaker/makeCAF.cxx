d737304f824b490b81904591c0c3f6d5fcb63922
Colin Weber <colweber@dunegpvm08.fnal.gov>
2026-03-10 16:49:23 -0500

    Fixed bug in makeCAF.cxx where the fill command was commented out. Also added a shell script to produce the ndfd-pairs where all of the energy depositions come from the ND geometry instead of LArbath.

--- inputs/ND_CAFMaker/makeCAF.cxx
+++ inputs/ND_CAFMaker/makeCAF.cxx
@@ -488,9 +488,10 @@ void loop( CAF &caf, params &par, TTree * tree, TTree * gtree, std::string fhicl
         }
       }
     }
-	if((caf.vtx_x < 310.) && (caf.vtx_x > -310.) && (caf.vtx_y < 550.) && (caf.vtx_y > -550.) && (caf.vtx_z < 1244.) && (caf.vtx_z > 50.) && ((caf.muon_tracker == 1) || (caf.muon_contained == 1)) && (caf.reco_numu == 1) && (((caf.reco_q == -1) && (caf.neutrinoPDG > 0)) || ((caf.reco_q == 1) && (caf.neutrinoPDG < 0))) && (caf.Ehad_veto < 30.)) {
+/*	if((caf.vtx_x < 310.) && (caf.vtx_x > -310.) && (caf.vtx_y < 550.) && (caf.vtx_y > -550.) && (caf.vtx_z < 1244.) && (caf.vtx_z > 50.) && ((caf.muon_tracker == 1) || (caf.muon_contained == 1)) && (caf.reco_numu == 1) && (((caf.reco_q == -1) && (caf.neutrinoPDG > 0)) || ((caf.reco_q == 1) && (caf.neutrinoPDG < 0))) && (caf.Ehad_veto < 30.)) {
         caf.fill();
-	}
+	}*/
+	caf.fill();
   }
 
   // set POT
